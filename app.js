9052c105d65d20ec8876866ae68dd6448dbb1493
Sami Awada <samiawada30@gmail.com>
2022-04-14 20:06:09 +0000

    installed axios

--- app.js
+++ app.js
@@ -8,8 +8,44 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+var request = require("request"); // "Request" library
 
-// view engine setup
+// Client ID 4d1a89c59c9c493a870c646af5bde69d
+// Client Secret 6294aabd768f4ab087ba2d16ae85d6fd
+
+var client_id = "4d1a89c59c9c493a870c646af5bde69d"; // Your client id
+var client_secret = "6294aabd768f4ab087ba2d16ae85d6fd"; // Your secret
+var authOptions = {
+  url: "https://accounts.spotify.com/api/token",
+  headers: {
+    Authorization:
+      "Basic " +
+      Buffer.from(client_id + ":" + client_secret).toString("base64"),
+  },
+  form: {
+    grant_type: "client_credentials",
+  },
+  json: true,
+};
+
+request.post(authOptions, function (error, response, body) {
+  if (!error && response.statusCode === 200) {
+    // use the access token to access the Spotify Web API
+    var token = body.access_token;
+    var options = {
+      url: "https://api.spotify.com/v1/users/jmperezperez",
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+      json: true,
+    };
+    request.get(options, function (error, response, body) {
+      console.log(body);
+    });
+  }
+});
+
+// view engine setup 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
