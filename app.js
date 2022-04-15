f13d80a7ca5f738859e6e702f6380596c97f15fc
Sami Awada <samiawada30@gmail.com>
2022-04-16 02:20:21 +0300

    comment spotify auth fun

--- app.js
+++ app.js
@@ -8,62 +8,29 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-var axios = require("axios");
-var qs = require("qs");
-require("dotenv").config();
-// Client ID 4d1a89c59c9c493a870c646af5bde69d
-// Client Secret 6294aabd768f4ab087ba2d16ae85d6fd
 
-const client_id = process.env.SPOTIFY_API_ID; // Your client id
-const client_secret = process.env.SPOTIFY_CLIENT_SECRET; // Your secret
-const auth_token = Buffer.from(
-  `${client_id}:${client_secret}`,
-  "utf-8"
-).toString("base64");
+// const getAudioFeatures_Track = async (track_id) => {
+//   //request token using getAuth() function
+//   const access_token = await getAuth();
+//   console.log(access_token);
 
-const getAuth = async () => {
-  try {
-    //make post request to SPOTIFY API for access token, sending relavent info
-    const token_url = "https://accounts.spotify.com/api/token";
-    const data = qs.stringify({ grant_type: "client_credentials" });
+//   const api_url = `https://api.spotify.com/v1/search?q=Bak&type=artist&limit=10`;
+//   // console.log(api_url);
+//   try {
+//     const response = await axios.get(api_url, {
+//       headers: {
+//         Authorization: `Bearer ${access_token}`,
+//         "Content-Type": "application/json",
+//       },
+//     });
+//     console.log(response.data.artists.items);
+//     return response.data;
+//   } catch (error) {
+//     console.log("GetArtistsError : ", error);
+//   }
+// };
 
-    const response = await axios.post(token_url, data, {
-      headers: {
-        Authorization: `Basic ${auth_token}`,
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-    });
-    //return access token
-    return response.data.access_token;
-    //console.log(response.data.access_token);
-  } catch (error) {
-    //on fail, log the error in console
-    console.log("ERROR-AUTH", error);
-  }
-};
-
-const getAudioFeatures_Track = async (track_id) => {
-  //request token using getAuth() function
-  const access_token = await getAuth();
-  console.log(access_token);
-
-  const api_url = `https://api.spotify.com/v1/search?q=Bak&type=artist&limit=10`;
-  // console.log(api_url);
-  try {
-    const response = await axios.get(api_url, {
-      headers: {
-        Authorization: `Bearer ${access_token}`,
-        "Content-Type": "application/json",
-      },
-    });
-    console.log(response.data.artists.items);
-    return response.data;
-  } catch (error) {
-    console.log("GetArtistsError : ", error);
-  }
-};
-
-console.log(getAudioFeatures_Track("07A0whlnYwfWfLQy4qh3Tq"));
+// console.log(getAudioFeatures_Track("07A0whlnYwfWfLQy4qh3Tq"));
 
 // view engine setup 
 
